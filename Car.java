2eb731e98d01777c7229c3278b29dc2b5c124ab4
TimothyMendezFeliciano <37247621+TimothyMendezFeliciano@users.noreply.github.com>
2019-10-15 16:37:26 -0400

    Add files via upload
    Initial Car class

---
+++ Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    private String model;
+    private int price;
+
+    public Car() {
+        this.model = "Generic";
+        this.price = 50;
+    }
+
+    public getModel() {
+        return this.model;
+    }
+
+    public getPrice() {
+        return this.price;
+    }
+}
